5b12084764dca02a56c24b8990ddb8b3c25a2957
BLANCO MATIAS <46876640@ort>
2023-06-14 15:57:39 -0300

    Added function to verify base64

--- app.py
+++ app.py
@@ -1,8 +1,18 @@
 from flask import Flask, request
+import re
 
 app = Flask("DesAIgner's Image and Links API")
 
 @app.post("/")
 def post():
-    data = request.json
+  data = request.get_data()
+  if not valid_encoded_image(data):
+    return "Input was not a valid base64 string for an image", 400
+  return data
 
+
+def valid_encoded_image(image):
+  regex = b'^(?:[A-Za-z0-9+\/]{4})*(?:[A-Za-z0-9+\/]{4}|[A-Za-z0-9+\/]{3}=|[A-Za-z0-9+\/]{2}={2})$'
+  metadata, data = image.split(b",")
+  match = re.match(regex, data)
+  return metadata == b'data:image/jpeg;base64' and match != None
